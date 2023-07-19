fcb19cd57ef63700adba72bff8ac7cec942e6562
facundo colonna <facundo@improntus.com>
2023-07-19 06:16:52 -0300

    1.0.1 - compatibilizo con nuevas versiones de composer

--- src/Plugin.php
+++ src/Plugin.php
@@ -276,4 +276,14 @@ class Plugin implements PluginInterface, EventSubscriberInterface
             }
         }
     }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+        // TODO: Implement deactivate() method.
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+        // TODO: Implement uninstall() method.
+    }
 }
